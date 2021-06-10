91179a25fc2abacd8f07bf22544dbec21026b185
Alexander M. Korn <dev@spume.net>
2021-06-10 16:26:32 +0200

    Fix no dir listing if no filter is given
    Update composer.json
    Update composer.json
    Update composer.json

--- src/fields/ServerPathFieldType.php
+++ src/fields/ServerPathFieldType.php
@@ -100,16 +100,27 @@ class ServerPathFieldType extends PlainText implements PreviewableFieldInterface
         if (is_dir($path)) {
 
             $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
+
+
             foreach ($iterator as $file) {
+
                 if ($file->isDir() && ($file->isDir() && $file->getFilename() != '..')) {
                     $p = $file->getPathname();
+
                     $p = str_replace(array($path, DIRECTORY_SEPARATOR . '.'), '', $p);
                     $p = ltrim($p, '/\\');
-                    if (!empty($p) && (!empty($settings['dirFilter']) && $this->strposa( $p, explode(',',$settings['dirFilter']) ) ))
+                    $isValid = true;
+                    if (!empty($settings['dirFilter'])) {
+                        $isValid = $this->strposa($p, explode(',', $settings['dirFilter']));
+                    }
+                    if (!empty($p) && $isValid) {
                         $dirList[] = $p;
+                    }
                 }
             }
+
             natsort($dirList);
+
             if (count($dirList) > 0) {
                 $string = '<div class="select"><select name="' . $this->handle .  '">';
                 $string .= '<option value="">Choose a directory</option>';
@@ -120,23 +131,26 @@ class ServerPathFieldType extends PlainText implements PreviewableFieldInterface
                 }
                 $string .= '</select></div>';
             } else {
-                $string="<p>No directories found.</p>";
+                $string = "<p>No directories found.</p>";
             }
         } else {
-            $string="<p>No directories found.</p>";
+            $string = "<p>No directories found.</p>";
         }
-
         return $string;
     }
 
-    function strposa($haystack, $needle, $offset=0) {
-        if(!is_array($needle)) $needle = array($needle);
-        foreach($needle as $query) {
-            if(strpos($haystack, $query, $offset) !== false) return true; // stop on first true result
+    function strposa($haystack, $needle, $offset = 0)
+    {
+        if (!is_array($needle)) {
+            $needle = array($needle);
+        }
+        foreach ($needle as $query) {
+            if (stripos($haystack, trim($query), $offset) !== false)
+                return true; // stop on first true result
         }
         return false;
     }
-    
+
     // Private Methods
     // =========================================================================
 
